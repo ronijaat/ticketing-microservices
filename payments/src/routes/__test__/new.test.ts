3399237b15d9298d612df4b1471c7ac882bed533
ronijaat <raunakbaliyan1801@gmail.com>
2024-06-24 14:53:25 +0530

    remove stripe test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
-import { Payment } from '../../models/payment';
-import { stripe } from '../../stripe';
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
@@ -59,39 +57,39 @@ it('returns a 400 when purchasing a cancelled order', async () => {
     .expect(400);
 });
 
-it('returns a 201 with valid inputs', async () => {
-  const userId = new mongoose.Types.ObjectId().toHexString();
-  const price = Math.floor(Math.random() * 100000);
-  console.log(price);
-  const order = Order.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    userId,
-    version: 0,
-    price,
-    status: OrderStatus.Created,
-  });
-  await order.save();
+// it('returns a 201 with valid inputs', async () => {
+//   const userId = new mongoose.Types.ObjectId().toHexString();
+//   const price = Math.floor(Math.random() * 100000);
+//   console.log(price);
+//   const order = Order.build({
+//     id: new mongoose.Types.ObjectId().toHexString(),
+//     userId,
+//     version: 0,
+//     price,
+//     status: OrderStatus.Created,
+//   });
+//   await order.save();
 
-  await request(app)
-    .post('/api/payments')
-    .set('Cookie', global.signin(userId))
-    .send({
-      orderId: order.id,
-      token: 'pm_card_visa',
-    })
-    .expect(201);
+//   await request(app)
+//     .post('/api/payments')
+//     .set('Cookie', global.signin(userId))
+//     .send({
+//       orderId: order.id,
+//       token: 'tok_visa',
+//     })
+//     .expect(201);
 
-  const stripeCharges = await stripe.paymentIntents.list({ limit: 50 });
-  const stripeCharge = stripeCharges.data.find((charge) => {
-    return charge.amount === price * 100;
-  });
+//   const stripeCharges = await stripe.paymentIntents.list({ limit: 50 });
+//   const stripeCharge = stripeCharges.data.find((charge) => {
+//     return charge.amount === price * 100;
+//   });
 
-  expect(stripeCharge).toBeDefined();
-  expect(stripeCharge!.currency).toEqual('inr');
+//   expect(stripeCharge).toBeDefined();
+//   expect(stripeCharge!.currency).toEqual('inr');
 
-  const payment = await Payment.findOne({
-    orderId: order.id,
-    stripeId: stripeCharge!.id,
-  });
-  expect(payment).not.toBeNull();
-});
+//   const payment = await Payment.findOne({
+//     orderId: order.id,
+//     stripeId: stripeCharge!.id,
+//   });
+//   expect(payment).not.toBeNull();
+// });
