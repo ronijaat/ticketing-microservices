3c50f83a4d4f8774254a5d1d73af52d88c01b354
ronijaat <raunakbaliyan1801@gmail.com>
2024-06-24 16:19:06 +0530

    test

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,7 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
-  console.log('check in');
+  console.log('check in all');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
