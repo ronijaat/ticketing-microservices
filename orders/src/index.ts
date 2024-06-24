6dc7bd6fb74705a9245eb9bb943c8c717fc15b09
ronijaat <raunakbaliyan1801@gmail.com>
2024-06-24 15:04:46 +0530

    fouth time

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,7 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
-  console.log('hi3');
+  console.log('hi4');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
